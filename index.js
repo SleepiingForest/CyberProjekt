f874cbbf870b15d254b3d50233f2812efb800385
SleepiingForest <quentin.bouchon@gmail.com>
2018-10-18 09:20:33 +0200

    Idk about this one

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
+var path = require('path');
+var publicDir = '/public';
+app.use(express.static('public'));
+app.use(publicDir, express.static('public'));
+
 
 app.get('/', function (req, res) {
-  res.send('Pouet!')
+  res.sendFile(path.join(__dirname, publicDir, 'index.html'));
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Cyber app listening on port 3000!')
 })
 
 
